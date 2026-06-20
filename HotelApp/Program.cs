2f1ec18881008cfd808fdac8b2118ab17879d0a7
Diego <diego88aht@gmail.com>
2026-06-20 11:19:33 -0400

    Habilitar swagger

--- HotelApp/Program.cs
+++ HotelApp/Program.cs
@@ -68,11 +68,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowFrontend");
